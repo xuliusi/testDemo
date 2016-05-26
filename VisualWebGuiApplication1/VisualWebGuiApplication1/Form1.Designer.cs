668125ee9709bc93ebbe5f30ff60ee0ce6e0e5d2
CL <CL@CLLX>
2016-05-26 10:29:20 +0800

    增加grpbox，增加checkbox2，checkbox3，labelxxx改名

--- VisualWebGuiApplication1/VisualWebGuiApplication1/Form1.Designer.cs
+++ VisualWebGuiApplication1/VisualWebGuiApplication1/Form1.Designer.cs
@@ -32,18 +32,23 @@ namespace VisualWebGuiApplication1
             this.listBox1 = new Gizmox.WebGUI.Forms.ListBox();
             this.label1 = new Gizmox.WebGUI.Forms.Label();
             this.checkBox1 = new Gizmox.WebGUI.Forms.CheckBox();
+            this.checkBox2 = new Gizmox.WebGUI.Forms.CheckBox();
+            this.textBox2 = new Gizmox.WebGUI.Forms.TextBox();
+            this.checkBox3 = new Gizmox.WebGUI.Forms.CheckBox();
+            this.groupBox1 = new Gizmox.WebGUI.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(158, 122);
+            this.textBox1.Location = new System.Drawing.Point(134, 52);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
             // 
             // listBox1
             // 
-            this.listBox1.Location = new System.Drawing.Point(175, 185);
+            this.listBox1.Location = new System.Drawing.Point(134, 85);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(100, 95);
             this.listBox1.TabIndex = 1;
@@ -51,29 +56,69 @@ namespace VisualWebGuiApplication1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(316, 151);
+            this.label1.Location = new System.Drawing.Point(369, 129);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "labelXXX";
             // 
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(341, 231);
+            this.checkBox1.Location = new System.Drawing.Point(100, 28);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(77, 17);
             this.checkBox1.TabIndex = 3;
             this.checkBox1.Text = "checkBox1";
             // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(372, 163);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(77, 17);
+            this.checkBox2.TabIndex = 3;
+            this.checkBox2.Text = "checkBox2";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(94, 65);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 0;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(100, 45);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(77, 17);
+            this.checkBox3.TabIndex = 4;
+            this.checkBox3.Text = "checkBox3";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.FlatStyle = Gizmox.WebGUI.Forms.FlatStyle.Flat;
+            this.groupBox1.Location = new System.Drawing.Point(134, 215);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
             // Form1
             // 
-            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox1);
             this.Size = new System.Drawing.Size(597, 425);
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -84,5 +129,9 @@ namespace VisualWebGuiApplication1
         private Gizmox.WebGUI.Forms.ListBox listBox1;
         private Gizmox.WebGUI.Forms.Label label1;
         private Gizmox.WebGUI.Forms.CheckBox checkBox1;
+        private Gizmox.WebGUI.Forms.CheckBox checkBox2;
+        private Gizmox.WebGUI.Forms.TextBox textBox2;
+        private Gizmox.WebGUI.Forms.CheckBox checkBox3;
+        private Gizmox.WebGUI.Forms.GroupBox groupBox1;
     }
 }
