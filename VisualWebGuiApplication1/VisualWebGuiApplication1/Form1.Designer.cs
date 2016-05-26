f1d6173bba7f9d9ffff0a1b1a4d5ea3955935f37
xuliusi <liusi10@qq.com>
2016-05-26 09:44:14 +0800

    的方式发生

---
+++ VisualWebGuiApplication1/VisualWebGuiApplication1/Form1.Designer.cs
@@ -0,0 +1,64 @@
+namespace VisualWebGuiApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Visual WebGui Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new Gizmox.WebGUI.Forms.TextBox();
+            this.listBox1 = new Gizmox.WebGUI.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(185, 131);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Location = new System.Drawing.Point(175, 185);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(100, 95);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Size = new System.Drawing.Size(597, 425);
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Gizmox.WebGUI.Forms.TextBox textBox1;
+        private Gizmox.WebGUI.Forms.ListBox listBox1;
+    }
+}
